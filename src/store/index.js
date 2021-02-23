cb1714c1bea0259d0bb5a803b6a40f17ebe4a7fb
ElielCatauro <ecatauronardella@alumnos.exa.unicen.edu.ar>
2021-02-23 14:20:03 -0300

    se agregaron comentarios

--- src/store/index.js
+++ src/store/index.js
@@ -13,35 +13,40 @@ export default new Vuex.Store({
     }
   },
   mutations: {
+    /* cargar tabla  */
     setGpus(state, payload) {
       state.gpus = payload.data;
     },
+    /* agregar elemento a tabla */
     addGpu(state, payload) {
       state.gpus.push(payload.gpu);
     },
+    /* editar elemento tabla  */
     editGpu(state, payload) {
       state.gpus = state.gpus.map((gpu) => { return gpu.id == payload.gpu.id ? payload.gpu : gpu; });
-
     },
+    /* eliminar elemento tabla  */
     deleteGpu(state, payload) {
       state.gpus = state.gpus.filter(gpu => gpu.id != payload.gpuID);
 
     },
+    /* Login --> state  */
     setUser(state, payload) {
       state.user = {
         token: payload.token,
         userId: payload.userId
       }
     },
+    /* Logout --> state */
     clearUser(state) {
       state.user = {};
     },
   },
   getters: {
+    /* getters tabla Gpus */
     allGpus(state) {
       return state.gpus;
     },
-    
     getSearchGpu: (state) => (El) => {
       let dataFiltrada = state.gpus;
       dataFiltrada = (El.GPU_Name === undefined) ? dataFiltrada : dataFiltrada.filter(e => e.GPU_Name.indexOf(`${El.GPU_Name}`) !== -1);
@@ -52,6 +57,7 @@ export default new Vuex.Store({
       dataFiltrada = (El.G2D_MarkMax === undefined) ? dataFiltrada : dataFiltrada.filter(e => e.G2D_Mark <= El.G2D_MarkMax);
       return dataFiltrada;
     },
+    /*getters login/logout  */
     getToken(state) {
       return state.user.token;
     },
@@ -61,15 +67,16 @@ export default new Vuex.Store({
     
   },
   actions: {
+    /* Llamado a las api Tabla y llama a la mutation para cargar el state*/
     fetchGpus({ commit }) {
       fetch("https://5fcbef5e51f70e00161f21d1.mockapi.io/gups")
         .then((res) => res.json())
         .then((data) => {
-          commit("setGpus", { data });
-  
+          commit("setGpus", { data }); 
         }
         );
     },
+    /* Post a firebase con email y pass del nuevo usuario */
     signUp({commit}, payload) {
       fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBTB0fKQnvOpVstth1WRHE8HcxhpSZ0MjI', {
         method: 'POST',
@@ -103,6 +110,7 @@ export default new Vuex.Store({
         })
       })
     },
+    /* Logout - limpiar logalStorage y llamar a mutation */
     logout({ commit }) {
       commit('clearUser');
       localStorage.setItem('token', null);
